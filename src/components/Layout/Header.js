97d7a89e1fbbc5547334b6b8de0ae6a7ce257bf7
Muhammad Awais <awaisaslampro@gmail.com>
2023-10-25 20:28:07 +0100

    Adding Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,16 @@
+import { Fragment } from "react";
+
 const Header = () => {
-    return(
-        <div>Header</div>
-    )
+    return<Fragment>
+<header>
+    <h1>RioMeals</h1>
+
+    <button>Cart</button>
+</header>
+<div>
+    <img src="https://www.pngall.com/wp-content/uploads/5/Buffet-PNG-Pic.png" alt="a delicious meals"/>
+</div>
+        </Fragment>;
 
 
 }
--- src/components/Layout/go.js
+++
@@ -1,2 +0,0 @@
-ldjldjf
-{v=j}
