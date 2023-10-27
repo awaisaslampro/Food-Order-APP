57d0cd2a249c581e54441a2edf708ea2c0d86d22
Muhammad Awais <awaisaslampro@gmail.com>
2023-10-27 20:43:22 +0100

    Applying Logic in Header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,11 +1,11 @@
-import { Fragment } from "react";
+import { Fragment} from "react";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 const Header = (props) => {
     return<Fragment>
 <header className={classes.header}>
     <h1>RioMeals</h1>
-    <HeaderCartButton/>
+    <HeaderCartButton onClick={props.onShowCart}/>
 </header>
 <div className={classes['main-image']}>
     <img src="https://raw.githubusercontent.com/academind/react-complete-guide-code/11-practice-food-order-app/extra-files/meals.jpg" alt="a delicious meals"/>
